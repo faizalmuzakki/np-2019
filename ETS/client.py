1151369b2130288476808429eceb3a41969276dc
Faizal Khilmi Muzakki <faizal.muzakki@gmail.com>
2019-03-27 14:01:57 +0700

    sek pegel

--- ETS/client.py
+++ ETS/client.py
@@ -4,64 +4,54 @@ import socket
 import threading
 import sys
 import os
+from ast import literal_eval
 
-class Client(threading.Thread):
-	def __init__(self, no):
-		self.iter = no
-		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		threading.Thread.__init__(self)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 9000
+sock.connect((host, port))
 
-	def run(self):
-		host = socket.gethostname()
-		port = 9000
-		while True:
-			command = raw_input('your command: ')
-			if command == "exit":
-				break
+name = raw_input('your name: ')
+while True:
+	command = raw_input('your command: ')
+	if command[0] == "exit":
+		break
 
-			self.my_socket.sendto(command, (host, port))
+	sock.send(command)
+	command = command.split(" ")
 
-			data, addr = self.my_socket.recvfrom(1024)
-			print '=========='
-			if str(data) == 'start_ls':
-				print 'showing all files'
-				print '=========='
-				while True:
-					data, addr = self.my_socket.recvfrom(1024)
-					if str(data) == 'stop_ls':
-						break
-					else:
-						print data
-			elif str(data) == 'start_download':
-				data, addr = self.my_socket.recvfrom(1024)
-				print "now downloading ", str(data)
-				filename = str(data)
-				print '=========='
-				file = open(data, 'wb+')
-				while True:
-					if str(data) == 'stop_download':
-						break
-					print "downloading progress ", str(data)
-					data, addr = self.my_socket.recvfrom(1024)
-					file.write(data)
-				file.close()
-			elif str(data) == 'error':
-				print "command not found"
-			else:
-				print str(data)
-			print '==========\n'
-		self.my_socket.close()
+	print '\n=========='
+	files = ''
+	if command[0] == 'ls':
+		print "showing files"
+		print '============='
 
-def main():
-	client1 = Client(1)
-	# client2 = Client(2)
-	# client3 = Client(3)
-	# client4 = Client(4)
+		data = sock.recv(1024)
+		files = literal_eval(data)
 
-	client1.start()
-	# client2.start()
-	# client3.start()
-	# client4.start()
+		for filename in files:
+			print filename
 
-if __name__=="__main__":
-	main()
+	elif command[0] == 'download':
+		if len(command) == 2:
+			print "downloading files"
+			print '================='
+
+			filename = name+'_'+command[1]
+			file = open(filename, 'wb')
+			data = sock.recv(1024)
+			while data:
+				print "downloading block: ", data
+				file.write(data)
+				data = sock.recv(1024)
+			file.close()
+			print "done receiving", filename
+
+		else:
+			print "syntax error"
+
+	else:
+		print "command not found"
+
+	print '==========\n'
+sock.close()
--- ETS/server.py
+++ ETS/server.py
@@ -3,52 +3,58 @@ import socket
 from threading import Thread
 import glob
 
+
+command = ["ls", "download"]
 files = glob.glob("*")
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((socket.gethostname(), 9000))
+sock.listen(1)
 
-def listFiles(ip, port):
-	addr = (ip, port)
-	sock.sendto('start_ls', addr)
-	for filename in files:
-		sock.sendto(filename, addr)
-	sock.sendto('stop_ls', addr)
-
-def downloadFiles(name, addr):
+def downloadFiles(conn, name, addr):
 	ip, port = addr
 	if name in files:
-		sock.sendto('start_download', addr)
-		sock.sendto(str(port)+'_'+name, addr)
-		file = open(name, 'rb')
-		file_data = file.read()
-		for byte in file_data:
-			sock.sendto(byte, addr)
-		sock.sendto('stop_download', addr)
+		with open(name, 'rb') as file:
+			print 'Sending... ', addr
+			bytes = file.read(1024)
+			while (bytes):
+				print 'Sending...', addr
+				conn.send(bytes)
+				bytes = file.read(1024)
+		file.close()
+
+class ProcessTheClient(Thread):
+	def __init__(self, connection, address):
+		self.connection = connection
+		self.address = address
+		Thread.__init__(self)
+
+	def run(self):
+		while True:
+			data = self.connection.recv(1024)
+			ip, port = self.address
+			data = str(data).split(' ')
+			print "{} from client {}".format(data[0], str(port))
+
+			if data[0] == "ls":
+				self.connection.send(str(files))
+
+			elif data[0] == 'download':
+				if len(data) == 2:
+					filename = data[1]
+					downloadFiles(self.connection, filename, self.address)
+				else:
+					print "syntax error"
+
+			elif data[0] not in command:
+				self.connection.send('error')
 
 def server():
-	command = ["hello", "ls", "download"]
 	while True:
 		print "waiting for request"
-		data, addr = sock.recvfrom(1024)
-		ip, port = addr
-		data = str(data).split(' ')
-		print "{} from client {}".format(data[0], str(port))
-		if data[0] == 'hello':
-			print "connection from {}:{}".format(ip, str(port))
-			sock.sendto('hello', addr)
-		elif data[0] == "ls":
-			thread = Thread(target=listFiles, args=(addr))
-			thread.start()
-		elif data[0] == 'download':
-			try:
-				filename = data[1]
-				thread = Thread(target=downloadFiles, args=(filename, addr))
-				thread.start()
-			except:
-				sock.sendto('error', addr)
-		elif data[0] not in command:
-			sock.sendto('error', addr)
+		conn, addr = sock.accept()
+		clt = ProcessTheClient(conn, addr)
+		clt.start()
 
 server()
